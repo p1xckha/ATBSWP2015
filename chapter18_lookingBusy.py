497f30260a9934775425b201c0c623150831b78b
p1xckha <125886152+p1xckha@users.noreply.github.com>
2023-02-23 23:42:20 +0900

    Add files via upload

--- chapter18_lookingBusy.py
+++ chapter18_lookingBusy.py
@@ -1,35 +1,39 @@
 # -*- coding: utf-8 -*-
 """
 Looking Busy
+
+find out whether PC is used or not in the following way:
+    mouse cursor was moved -> PC is used
+    mouse cursor was not moved -> PC is not used
 """
 
 import pyautogui
 import time
 from math import sqrt
+import numpy as np
+
 
 class MouseCursor():
-    SHORT_DISTANCE = 1000
+    SHORT_DISTANCE = 10
     def __init__(self):
-        self.interval = 1 # 
-        self.DURATION = 60 # 
-        self.recent_distance = 0
+        self.interval = 1 # seconds
+        self.DURATION = 60 # seconds
+        self.recent_distance = 0 # distance which mouse moved in recent DURATION
         self.positions = None # list of (x, y) as circular buffer
-        self.latest_positon_index = -1
+        self.buffersize = None
         self.initialize_positions()
+        self.total_time = 0
     
     def initialize_positions(self):
         buffersize = int (self.DURATION / self.interval)
-        self.positions = [None] * buffersize
+        self.positions = np.zeros((buffersize, 2))
+        self.buffersize = buffersize
         
     def add_position(self):
         position = pyautogui.position()
-        self.move_latest_position_index()
-        i = self.latest_positon_index
-        self.positions[i] = position
+        self.positions = np.roll(self.positions, 1, axis=0) # shift one element
+        self.positions[0] = position # override oldest element with position
     
-    def move_latest_position_index(self):
-        self.latest_positon_index = (self.latest_positon_index + 1) % self.DURATION
-        
     def distance(self, pos1, pos2):
         x1, y1 = pos1
         x2, y2 = pos2
@@ -37,18 +41,14 @@ class MouseCursor():
         return distance
     
     def set_recent_distance(self):
-        # calculate the total distance mouse move in recent 10 mins
-        recent = 0
-        i = self.latest_positon_index
-        while self.positions[i] != None and self.positions[i-1] != None:
+        # calculate the distance mouse moved in resent DURATION
+        recent_distance = 0
+        for i in range(1, self.buffersize):
             pos1 = self.positions[i-1]
             pos2 = self.positions[i]
             distance = self.distance(pos1, pos2)
-            recent += distance
-            i = (i-1) % int(self.DURATION / self.interval)
-            if i == self.latest_positon_index:
-                break
-        self.recent_distance = recent
+            recent_distance += distance
+        self.recent_distance = recent_distance
     
     def get_recent_distance(self):
         return self.recent_distance
@@ -57,28 +57,23 @@ class MouseCursor():
         print(self.positions)
     
     def is_many_positons(self):
-        i = self.latest_positon_index
-        amount = 0
-        while self.positions[i] != None:
-            amount += 1
-            i = (i-1) % int (self.DURATION / self.interval)
-            if i == self.latest_positon_index:
-                break
-        
-        return amount == int (self.DURATION / self.interval)
+        # if self.positions are full, we get enough positions  
+        return self.total_time >= self.buffersize
     
     def is_long_recent_distance(self):
+        # if mouse moves, I assume PC is being used now
         return self.get_recent_distance() > self.SHORT_DISTANCE
     
     def handle_positions(self):
         self.add_position()
+        self.total_time += self.interval
         self.set_recent_distance()
         
-    def isused(self):
+    def is_used(self):
         if self.is_many_positons() and not self.is_long_recent_distance():
-            return False
+            return False # mouse is not used
         else:
-            return True
+            return True # mouse is used
     
     def run(self):
         print("*" * 20)
@@ -87,7 +82,7 @@ class MouseCursor():
         try:
             while True:
                 self.handle_positions()
-                if self.isused():
+                if self.is_used():
                     print("You are using PC now. ", end="")
                 else:
                     print("You are not using PC now. ", end="")
